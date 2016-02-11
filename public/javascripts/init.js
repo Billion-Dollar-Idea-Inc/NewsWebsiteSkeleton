4c7ee52f9787ae5fde02af7cd34ebd85f4765c40
Austin Ward <ward.austin28@gmail.com>
2016-02-11 14:51:48 -0500

    Node Skeleton

---
+++ public/javascripts/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.modal-trigger').leanModal(); 
+    $('#modal1').openModal();
+    $('#modal1').closeModal();
+  });// end of document ready
+})(jQuery); // end of jQuery name space
